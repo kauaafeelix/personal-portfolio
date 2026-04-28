aefd9b3f94b37cc437648c1daac36f7a7b5f70ce
Kauã Felix <kfelixcosta11@gmail.com>
2026-04-28 18:18:03 -0300

    feat: adiciona confuguracoes do tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        space: ["var(--font-space-grotesk)"],
+        inter: ["var(--font-inter)"],
+      },
+    },
+  },
+};
